aceebf3dbf1649558ad6b7044cf1020b705045c3
Suren1997 <surendar16cool@gmail.com>
2026-06-28 15:29:55 +0530

    adding ignore files

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request, jsonify
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_AI_KEY")
+)
 
 app = Flask(__name__)
 
@@ -7,15 +15,32 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/chat", methods=["POST"])
 def chat():
-    data = request.get_json()
-    print(data)
-    user_message = data.get("message","")
+    try:
+        data = request.get_json()
 
-    reply = f"you said: {user_message}"
+        user_message = data.get("message", "")
 
-    return jsonify({"reply":reply})
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=user_message
+        )
 
-if __name__ == '__main__':
+        reply = response.text
+
+        return jsonify({
+            "reply": reply
+        })
+
+    except Exception as e:
+        print("ERROR:", e)
+
+        return jsonify({
+            "reply": f"Error: {str(e)}"
+        }), 500
+
+
+if __name__ == "__main__":
     app.run(debug=True)
