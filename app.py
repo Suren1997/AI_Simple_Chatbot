03eaf530f6788d3806acaaa85163711a9ab801df
Suren1997 <surendar16cool@gmail.com>
2026-06-28 12:58:09 +0530

    adding javascript functionality

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
 
 app = Flask(__name__)
 
@@ -6,6 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+    print(data)
+    user_message = data.get("message","")
+
+    reply = f"you said: {user_message}"
+
+    return jsonify({"reply":reply})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/chat.js
+++ static/chat.js
@@ -4,22 +4,37 @@ let sendButton = document.getElementById("send-btn");
 
 function addMessage(text, who) {
   let newMessage = document.createElement("div");
-  newMessage.className = "mesaage" + who;
+  newMessage.className = "message" + who;
   newMessage.innerText = text;
   chatBox.appendChild(newMessage);
+  chatBox.scrollTop = chatBox.scrollHeight;
 }
 
 function sendMessage() {
-  let text = messageInput.ariaValueMax;
+  let text = messageInput.value;
 
-  if (text == "") {
+  if (text === "") {
     return;
   }
 
   addMessage(text, "user");
+
   messageInput.value = "";
 
-  addMessage("I hear you! we will connect this to flask max", "bot");
+  // addMessage("I hear you! we will connect this to flask max", "bot");
+  fetch("/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: text }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      addMessage(data.reply, "bot");
+    });
 }
 
 sendButton.onclick = sendMessage;
